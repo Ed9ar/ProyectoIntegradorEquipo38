6ef58330a72b59359a16bfb6469c2b58aec4a506
Ruben M. Ramos <67209988+MarcosGuerrerou@users.noreply.github.com>
2024-05-15 09:24:26 -0600

    Add files via upload

---
+++ porcupine_test/test.py
@@ -0,0 +1,46 @@
+import os
+import struct
+import pyaudio
+import pvporcupine
+from datetime import datetime
+
+porcupine = None
+pa = None
+audio_stream = None
+
+def main():
+    try:
+        porcupine = pvporcupine.create(access_key=os.environ['PORCUPINE_ACCESS_KEY'],
+                                    keyword_paths=['/Users/marcosguerrero/Documents/MNA/probando/Hola-te-bot_es_mac_v3_0_0.ppn'],
+                                    model_path='/Users/marcosguerrero/Documents/MNA/probando/porcupine_params_es.pv',
+                                    sensitivities=[0.9])
+
+        pa = pyaudio.PyAudio()
+
+        audio_stream = pa.open(
+                        rate=porcupine.sample_rate,
+                        channels=1,
+                        format=pyaudio.paInt16,
+                        input=True,
+                        frames_per_buffer=porcupine.frame_length)
+
+        print(f"{datetime.now()} - Listening!")
+        
+        while True:
+            pcm = audio_stream.read(porcupine.frame_length)
+            pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+
+            keyword_index = porcupine.process(pcm)
+
+            if keyword_index >= 0:
+                print(f"{datetime.now()} - 'Hola TecBot' detectado")
+    except KeyboardInterrupt:
+        print(f"{datetime.now()} - Stopped listening!")
+        exit()
+    except Exception as e:
+        exit()
+    finally:
+        porcupine.delete()
+
+if __name__ == '__main__':
+    main()
